91207dbd923b30267b4a2cf6eb6760588ff3e343
eunsung <eunsung.shin@gmail.com>
2023-04-12 10:56:53 +0900

    hydra instantiation for big picture;formatting for v1-v8

--- my_app.py
+++ my_app.py
@@ -4,6 +4,16 @@ from omegaconf import DictConfig
 @hydra.main(config_path ='config', config_name = 'config')
 def my_app(cfg: DictConfig) -> None :
     print(cfg)
+    '''
+    ref : https://hydra.cc/docs/advanced/instantiate_objects/overview/
+    instantiate
+
+    model
+    datamodule
+    optimizer
+    trainer
+
+    '''
 
 if __name__ == '__main__' :
     my_app()
