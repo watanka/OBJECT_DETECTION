b82f2180792da7bd271ec73c841d18c7af05a2bd
eunsungshin <eunsung.shin@gmail.com>
2023-04-14 15:59:01 +0900

    debug yolov1 loss:pr(obj),x,y,w,h

--- YOLOv2/data.py
+++ YOLOv2/data.py
@@ -1,3 +1,5 @@
+import hydra
+
 import os
 import json
 from tqdm import tqdm
@@ -34,42 +36,59 @@ label_dict = {num: clsname for clsname, num in classes_dict.items()}
 
 
 class BDDDataModule(pl.LightningDataModule) :
-    def __init__(self, imgdir, jsonfile, num_grid, num_classes, anchorbox, batch_size, num_workers, 
+    def __init__(self, 
+                 cfg, 
                  train_transform = None,
                  test_transform = None,
                  predict_transform = None
                  ) :
         super().__init__()
-        self.imgdir = imgdir
-        self.jsonfile = jsonfile
-        self.num_grid = num_grid
-        self.num_classes = num_classes
-        self.anchorbox = anchorbox
+        self.cfg = cfg
 
-        self.batch_size = batch_size
-        self.num_workers = num_workers
+        self.train_imgdir = cfg.dataset.train.imgdir
+        self.train_jsonfile = cfg.dataset.train.jsonfile
+
+        self.test_imgdir = cfg.dataset.test.imgdir
+        self.test_jsonfile = cfg.dataset.test.jsonfile
+
+        self.predict_imgdir = cfg.dataset.predict.imgdir
+        self.predict_jsonfile = cfg.dataset.predict.jsonfile
+
+        self.num_grid = cfg.model.num_grid
+        self.num_classes = cfg.model.num_classes
+        self.anchorbox = cfg.model.anchorbox
+
+        self.batch_size = cfg.dataset.batch_size
+        self.num_workers = cfg.dataset.num_workers
         self.train_transform = train_transform
         self.test_transform = test_transform
         self.predict_transform = predict_transform
 
     def setup(self, stage : str) :
         if stage == 'fit' :
-            self.train_dataset = BDDDataset(self.imgdir, 
-                                            self.jsonfile, 
+            self.train_dataset = BDDDataset(self.train_imgdir, 
+                                            self.train_jsonfile, 
                                             self.num_grid, 
                                             self.num_classes, 
                                             self.anchorbox, 
                                             is_train = True, transform = self.train_transform)
-        if stage == 'test' :
-            self.test_dataset = BDDDataset(self.imgdir, 
-                                            self.jsonfile, 
+
+            self.test_dataset = BDDDataset(self.test_imgdir, 
+                                            self.test_jsonfile, 
+                                            self.num_grid, 
+                                            self.num_classes, 
+                                            self.anchorbox, 
+                                            is_train = True, transform = self.test_transform)
+        if stage == 'validate' or stage == 'test' :
+            self.test_dataset = BDDDataset(self.test_imgdir, 
+                                            self.test_jsonfile, 
                                             self.num_grid, 
                                             self.num_classes, 
                                             self.anchorbox, 
                                             is_train = True, transform = self.test_transform)
         if stage == 'predict' :
-            self.predict_dataset = BDDDataset(self.imgdir, 
-                                            self.jsonfile, 
+            self.predict_dataset = BDDDataset(self.predict_imgdir, 
+                                            self.predict_jsonfile, 
                                             self.num_grid, 
                                             self.num_classes, 
                                             self.anchorbox, 
@@ -78,7 +97,7 @@ class BDDDataModule(pl.LightningDataModule) :
     def train_dataloader(self) :
         return DataLoader(self.train_dataset, batch_size = self.batch_size, num_workers = self.num_workers)
     def val_dataloader(self) :
-        return DataLoader(self.train_dataset, batch_size = self.batch_size, num_workers = self.num_workers)
+        return DataLoader(self.test_dataset, batch_size = self.batch_size, num_workers = self.num_workers)
     def test_dataloader(self) :
         return DataLoader(self.test_dataset, batch_size = self.batch_size, num_workers = self.num_workers)
     def predict_dataloader(self) :
@@ -123,7 +142,7 @@ class BDDDataset(Dataset):
 
     def __getitem__(self, idx):
         imgfile = self.imgfiles[idx]
-        image = plt.imread(imgfile) #/ 255.0
+        image = cv2.imread(imgfile) #/ 255.0
 
         if self.is_train :
             json_info = self.json_infos[idx]
@@ -172,10 +191,9 @@ class BDDDataset(Dataset):
 
     def encode(self, bboxes, labels, H, W):
         # yolov2 update : (num_grid, num_grid, anchorbox * (x,y,w,h,pr(obj), num_classes)). each anchor box predicts separate class
+        # yolov2 update : separate array for each anchor box
         label_grid = np.zeros(
-            (self.num_grid, self.num_grid, len(self.anchorbox) * (5 + self.num_classes) ),
-            np.float32,
-        )
+            (self.num_grid, self.num_grid, len(self.anchorbox), 5 + self.num_classes ), np.float32,)
         
         grid_idxbox = np.zeros((self.num_grid, self.num_grid), np.uint8)
 
@@ -205,9 +223,27 @@ class BDDDataset(Dataset):
 
             boxnum = grid_idxbox[grid_yidx][grid_xidx]
             if boxnum < len(self.anchorbox) : # if total number of boxes exceeds len(anchorbox), then ignore.
-                label_grid[grid_yidx, grid_xidx, boxnum * ( 5 + self.num_classes ) : boxnum * ( 5 + self.num_classes ) + 5 ] = [1, normalized_x, normalized_y, w, h]
-                label_grid[grid_yidx, grid_xidx, boxnum * ( 5 + self.num_classes ) + label] = 1
+                label_grid[grid_yidx, grid_xidx, boxnum, 5] = [1, normalized_x, normalized_y, w, h]
+                label_grid[grid_yidx, grid_xidx, boxnum, 5 + label] = 1
 
                 grid_idxbox[grid_yidx][grid_xidx] += 1
             
         return np.float32(label_grid)
+
+
+if __name__ == '__main__' :
+    from hydra import compose, initialize
+    from omegaconf import OmegaConf
+
+    initialize(config_path="../config", job_name="test datamodule")
+    cfg = compose(config_name="../config")
+
+    datamodule = BDDDataModule(cfg)
+
+    datamodule.setup('fit')
+
+    train_dataloader = datamodule.train_dataloader()
+
+    img_batch, label_batch = next(iter(train_dataloader))
+
+    
